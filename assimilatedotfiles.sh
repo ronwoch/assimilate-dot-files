cfbcceac43fb0084735aa0dbf18bf0f702e5fa2e
don.mac <dgauss@localhost.localdomain>
2019-06-04 11:47:52 +0000

    initial commit

---
+++ assimilatedotfiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#=========================================================================================
+# assimilatedotfiles.sh
+# Ronald Wochner 
+# Tue 04 Jun 2019 11:35:55 AM UTC
+# Version 1
+# Simple bash script to pull dotfiles and set them up
+#=========================================================================================
+
+repo=$1
+
+# Check that all args are given
+if [ $# -lt 1 ]
+then 
+    echo "Usage: $0 \"repo path"
+    exit
+fi
+
+
+
+git clone --bare $repo $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
