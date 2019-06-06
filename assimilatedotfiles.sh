654a07f2bc3b564e85b4428cd83336721a5e4e3b
dGauss <dGauss>
2019-06-06 16:51:51 -0700

    updated

--- assimilatedotfiles.sh
+++ assimilatedotfiles.sh
@@ -30,6 +30,7 @@ if [ $? = 0 ]; then
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    echo "Backup complete, checking out files"
 fi;
 config checkout
 config config status.showUntrackedFiles no
